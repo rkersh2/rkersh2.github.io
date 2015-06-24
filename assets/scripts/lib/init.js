df55f1bf4fce24f0e93986654730c18e1f458125
Ryan <rkersh2@gmail.com>
2015-06-24 12:35:26 -0400

    Basic configuration and set to point to CNAME

--- assets/scripts/lib/init.js
+++ assets/scripts/lib/init.js
@@ -11,9 +11,9 @@ exports.init = function() {
         logo.initLogo(canvas.targetContext);
         var pixels = Array.prototype.slice.call(canvas.targetContext.getImageData(0, 0, canvas.width, canvas.width).data);
         var index;
-        for (var y = 0; y < canvas.width; y += 3) {
-            for (var x = 0; x < canvas.width; x += 3) {
-                index = (y * canvas.width + x) * 4;
+        for (var y = 0, z = canvas.width; y < z; y += 3) {
+            for (var x = 0; x < z; x += 3) {
+                index = (y * z + x) * 4;
                 if(pixels[++index] > 0) {
                     canvas.particleArr.push(new Particle(x, y, 0, 0));
                 }
@@ -21,7 +21,7 @@ exports.init = function() {
         }
         function make() {
             canvas.context.clearRect(0, 0, canvas.width, canvas.width);
-            for(var i = 0; i < canvas.particleArr.length; i++) {
+            for(var i = 0, j = canvas.particleArr.length; i < j; i++) {
                 var p = canvas.particleArr[i];
                 canvas.context.fillRect(p.x, p.y, 2, 2);
             }
