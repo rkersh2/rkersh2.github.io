8ddf8225a13752dd63dacc520f497b308bced4dd
Ryan <rkersh2@gmail.com>
2015-07-12 20:42:54 -0400

    window reload on mobile screen flip

--- assets/scripts/lib/canvas.js
+++ assets/scripts/lib/canvas.js
@@ -35,6 +35,9 @@ document.body.addEventListener("touchend", function(event) {
     exports.mouse.x = 1;
     exports.mouse.y = 1;
 }, false);
+window.onresize = function(event) {
+    document.location.reload(true);
+};
 exports.mouseRadius = Math.pow(100, 2);
 exports.friction = 0.95;
 exports.ease = 0.25;
